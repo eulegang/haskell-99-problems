4cdcc71d787b2db1a93689d220673e055349f34c
eulegang <eulegang@eulegang.dev>
2022-04-06 20:42:34 -0500

    First to problems

--- src/List.hs
+++ src/List.hs
@@ -1,7 +1,18 @@
 module List (
-myLast
+myLast,
+myButLast
 ) where 
 
 myLast :: [a] -> Maybe a
-myLast = undefined
+myLast [] = Nothing
+myLast [x] = Just x
+myLast (_:ls) = myLast ls
+
+myButLast :: [a] -> Maybe a
+myButLast [] = Nothing
+myButLast (x:xs) = inner x xs
+  where
+    inner _ [] = Nothing
+    inner a [b] = Just a
+    inner _ (x:xs) = inner x xs
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,25 @@
 import Test.Hspec
 
+import List
+
 main :: IO ()
 main = hspec $ do
+  context "first 10 questions" $ do
+    describe "1) myLast" $ do
+      it "last int" $ do
+        List.myLast [1, 2, 3, 4] `shouldBe` Just 4
+      it "last string" $ do
+        List.myLast ['x', 'y', 'z'] `shouldBe` Just 'z'
+      it "last nothing" $ do
+        List.myLast [] `shouldBe` (Nothing :: Maybe Int)
 
-  it "should work" $ do
-    1 `shouldBe` 1
+    describe "2) myButLast" $ do
+      it "butLast int" $ do
+        List.myButLast [1, 2, 3, 4] `shouldBe` Just 3
+      it "butLast One element" $ do
+        List.myButLast [1] `shouldBe` Nothing
+      it "butLast zero elements" $ do
+        List.myButLast [] `shouldBe` (Nothing :: Maybe Int)
+        
+
+
